2787b359bcffe581c4112b0a8f4607a0ecf65375
Melisa <caspertendam@github.com>
2020-09-17 12:54:05 +0200

    start repository in social_interaction

---
+++ src/configuration.py
@@ -0,0 +1,8 @@
+import os
+
+#%% ENVIRONMENT VARIABLES
+os.environ['PROJECT_DIR_LOCAL'] = '/home/sebastian/Documents/Melisa/social_interaction/'
+os.environ['DATA_DIR_LOCAL'] = '/mnt/Data01/data/social_interaciton/'
+
+os.environ['LOCAL_USER'] = 'sebastian'
+os.environ['ANALYST'] = 'Meli'
